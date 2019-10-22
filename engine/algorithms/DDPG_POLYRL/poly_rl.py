365f4285520fc79ba9f6a43c286af19dac43e01c
habout <hossain.aboutalebi@gmail.com>
2019-10-22 18:17:49 -0400

    Fixed bugs DDPG

--- engine/algorithms/DDPG_POLYRL/poly_rl.py
+++ engine/algorithms/DDPG_POLYRL/poly_rl.py
@@ -48,35 +48,46 @@ class PolyRL():
             k = random.uniform(0, 1)
             if (k <= self.epsilon):
                 self.number_of_time_target_policy_is_called += 1
-                tensor_board_writer.add_scalar('number_of_time_target_policy__exploration_is_called',
-                                               self.number_of_time_target_policy_is_called / step_number,
-                                               step_number)
+                self.reset_parameters_PolyRL()
+                #print('target is called (After target policy having been called before)')
+                #tensor_board_writer.add_scalar('number_of_time_target_policy__exploration_is_called',
+                #                               self.number_of_time_target_policy_is_called / (step_number + 1),
+                #                               step_number + 1)
                 action = self.actor_target_function(state)
                 action = action
             else:
                 self.should_use_target_policy = False
+                #print('PolyRL is called (After target policy having been called)')
                 self.eta = abs(np.random.normal(self.lambda_, np.sqrt(self.sigma_squared)))
                 action = self.sample_action_algorithm(previous_action)
         elif (self.t == 0):
             action = torch.Tensor(1, self.nb_actions).uniform_(self.min_action_limit, self.max_action_limit)
+            #print('first action is selected at random (must happen ONLY at the beginning of each episode)')
 
         elif (((self.delta_g <= self.U) and (self.delta_g >= self.L) and (self.C_theta > 0)) or self.i == 1):
+            #print('PolyRL is called (delta_g is in range or i =1)')
             self.eta = abs(np.random.normal(self.lambda_, np.sqrt(self.sigma_squared)))
             action = self.sample_action_algorithm(previous_action)
 
         else:
             self.number_of_time_target_policy_is_called += 1
-            tensor_board_writer.add_scalar('number_of_time_target_policy__exploration_is_called', self.number_of_time_target_policy_is_called,
-                                           step_number)
+            #print('target is called (delta_g is NOT in range)')
+            #tensor_board_writer.add_scalar('number_of_time_target_policy__exploration_is_called', self.number_of_time_target_policy_is_called / (step_number + 1),
+            #                               step_number + 1)
             action = self.actor_target_function(state)
             self.reset_parameters_PolyRL()
             self.should_use_target_policy = True
             action = action
+        #if step_number > 0:
+        #    print('target_percentage = ', self.number_of_time_target_policy_is_called/(step_number+1))
+        tensor_board_writer.add_scalar('Percentage_target_policy__exploration_is_called',
+                                       self.number_of_time_target_policy_is_called / (step_number + 1),
+                                       step_number + 1)
         return torch.Tensor(action)
 
     # This function resets parameters of PolyRl every episode. Should be called in the beggining of every episode
     def reset_parameters_in_beginning_of_episode(self, episode_number):
-        self.should_use_target_policy = False
+        #self.should_use_target_policy = False
         self.epsilon = 1 - np.exp(-self.betta * episode_number)
         self.i = 1
         self.g = 0
@@ -106,8 +117,16 @@ class PolyRL():
             action = Q
         else:
             action = -Q
+        #print('previous_action = ', previous_action)
+        #print('action = ', action)
+        #action_dot_product = np.dot(previous_action.reshape(-1), action.reshape(-1))
+        #print('action_dot_product = ', action_dot_product)
+        #norm_action =  np.linalg.norm(action, ord=2)
+        #cos_action = action_dot_product/(norm_previous_action * norm_action)
+        #print('cos_action = ', cos_action)
         action = np.clip(action.numpy(), self.min_action_limit, self.max_action_limit)
         self.i += 1
+        #print('i = ', self.i)
         return torch.from_numpy(action)
 
     def update_parameters(self, previous_state, new_state, tensor_board_writer=None):
@@ -125,6 +144,7 @@ class PolyRL():
             self.C_theta = ((self.i - 2) * self.C_theta + torch.dot(self.w_new.reshape(-1),
                                                                     self.w_old.reshape(-1)).item() / (
                                     norm_w_new * norm_w_old)) / (self.i - 1)
+            #print('c_theta = ', self.C_theta)
             K = 0
             for j in range(1, self.i):
                 if (self.C_theta == 1):
@@ -135,26 +155,31 @@ class PolyRL():
             last_term = (1 / (self.i - 1)) * self.old_g
 
             # Upper bound and lower bound are computed here
-            self.U = (1 / ((self.i ** 3) * (1 - self.epsilon))) * (
+            self.U = (1 / ((self.i ** 3) * (self.epsilon))) * (
                     (self.i ** 2) * self.b + (norm_B_vector ** 2) + 2 * self.i * self.b * K) - last_term
 
-            tensor_board_writer.add_scalar('Upper_bound_PolyRL', self.U, self.number_of_time_PolyRL__update_parameter_is_called)
+            #tensor_board_writer.add_scalar('Upper_bound_PolyRL', self.U, self.number_of_time_PolyRL__update_parameter_is_called)
 
-            self.L = (1 - np.sqrt(2 * self.epsilon)) * (
+            self.L = (1 - np.sqrt(2 * (1-self.epsilon))) * (
                     self.b / self.i + (((self.i - 1) * (self.i - 2)) / self.i ** 2) * self.b * self.compute_correlation_decay() + (
                     1 / self.i ** 3) * norm_B_vector ** 2) - last_term
-
-            self.L = max(0, self.L)
+            #self.L = max(0, self.L)
+            if self.L != self.L :
+                self.L = -1
+            else:
+                tensor_board_writer.add_scalar('Delta_bounds_U-L_PolyRL', self.U - self.L,
+                                               self.number_of_time_PolyRL__update_parameter_is_called)
 
             # logger.info("@Upper bound value: {}, @Lower bound value: {}".format(self.U,self.L))
 
-            tensor_board_writer.add_scalar('Lower_bound_PolyRL', self.L, self.number_of_time_PolyRL__update_parameter_is_called)
-            tensor_board_writer.add_scalar('Delta_bounds_PolyRL', self.U - self.L, self.number_of_time_PolyRL__update_parameter_is_called)
+            #tensor_board_writer.add_scalar('Lower_bound_PolyRL', self.L, self.number_of_time_PolyRL__update_parameter_is_called)
+            #tensor_board_writer.add_scalar('Delta_bounds_PolyRL', self.U - self.L, self.number_of_time_PolyRL__update_parameter_is_called)
 
         self.number_of_time_PolyRL__update_parameter_is_called += 1
         self.b = ((self.i - 1) * self.b + norm_w_new ** 2) / self.i
         self.C_vector = ((self.i - 1) * self.C_vector + torch.Tensor(previous_state)) / self.i
         self.t += 1
+        #print('t = ',self.t)
 
     def compute_correlation_decay(self):
         if (self.C_theta == 1):
@@ -174,6 +199,6 @@ class PolyRL():
         self.C_theta = 0.001
         self.L = -1
         self.U = 1
-        self.t = 0
-        self.w_new = torch.zeros(self.nb_observations)
-        self.w_old = torch.zeros(self.nb_observations)
+        #self.t = 0
+        #self.w_new = torch.zeros(self.nb_observations)
+        #self.w_old = torch.zeros(self.nb_observations)
