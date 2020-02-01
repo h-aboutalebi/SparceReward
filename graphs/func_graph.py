9a2d54a8be97811283d5963d558cf4d4bdc87ab5
habout <hossain.aboutalebi@gmail.com>
2020-01-31 22:55:09 -0500

    Added graphics

--- graphs/func_graph.py
+++ graphs/func_graph.py
@@ -1,6 +1,43 @@
 import pickle
 from scipy.ndimage.filters import gaussian_filter1d
 import numpy as np
+from glob import glob
+import matplotlib as plt
+import math
+
+
+def create_graph(target,plt_name,y_label,x_label,result_folders,colors):
+    dict_results = {}
+    line_names = []
+    initilize_plt_conf(plt,y_label,x_label)
+    min_x_axis = math.inf
+    for c, folder in enumerate(result_folders):
+        name_folder = folder.split("/")[-2]
+        line_names.append(name_folder)
+        min_y_axis = math.inf
+        results = []
+        files = glob(folder + "/*.pkl")
+        x = get_x(files[0])
+        for file_path in files:
+            last_x, last_y = get_result_file(results, file_path,target)
+            if (last_x < min_x_axis):
+                min_x_axis = last_x
+            if (last_y < min_y_axis):
+                min_y_axis = last_y
+        x = x[:min_y_axis]
+        normalized_results = []
+        for data in results:
+            normalized_results.append(data[:min_y_axis])
+        mean = np.mean(np.array(normalized_results), axis=0)
+        std = np.std(np.array(normalized_results), axis=0)
+        plt.plot(x, mean, colors[c], label=name_folder)
+        plt.fill_between(x, mean - std, mean + std, edgecolor=colors[c],
+                         facecolor=colors[c], alpha=0.21,
+                         linewidth=0)
+        dict_results[name_folder] = {"mean": mean, "std": std}
+    plt.legend(loc='upper left')
+    axes = plt.gca()
+    plt_name.show()
 
 def get_x(path_file):
     with open(path_file, 'rb') as f:
@@ -8,13 +45,16 @@ def get_x(path_file):
     x = [x["step_nb"] for x in example_dict]
     return x
 
-def get_result_file(results,path_file):
+def get_result_file(results,path_file,target,smoothness=True):
     with open(path_file, 'rb') as f:
         example_dict = pickle.load(f)
-    mod_reward=[x["mod_reward"] for x in example_dict]
-    y=make_smooth_line(mod_reward)
+    y_values=[x[target] for x in example_dict]
+    if(smoothness):
+        y=make_smooth_line(y_values)
+    else:
+        y=y_values
     results.append(y)
-    return max_step_nb(example_dict),len(mod_reward)
+    return max_step_nb(example_dict),len(y_values)
 
 def make_smooth_line(list):
     ysmoothed = gaussian_filter1d(list, sigma=1)
@@ -23,7 +63,9 @@ def make_smooth_line(list):
 def max_step_nb(example_dict):
     return (example_dict[-1]['step_nb']-example_dict[-2]['step_nb']-1)*(len(example_dict)+1)
 
-def initilize_plt_conf(plt):
-    plt.xlabel('steps')
-    plt.ylabel('reward')
+def initilize_plt_conf(plt,y_label,x_label):
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+
+
--- graphs/multi_graphs.py
+++ graphs/multi_graphs.py
@@ -5,44 +5,18 @@ from scipy.stats import sem
 import matplotlib.pyplot as plt
 
 # directory path that contains all the folder of pkl files
-from graphs.func_graph import get_result_file, get_x, initilize_plt_conf
+from graphs.func_graph import get_result_file, get_x, initilize_plt_conf, create_graph
 
 directory_path = "/Users/hosseinaboutalebi/Desktop/myfigures"
-colors = ['b', 'g', 'r', 'm', 'y', 'c']
 
+#### performance graphs ####
 result_folders = glob(directory_path + "/*/")
-dict_results = {}
-line_names = []
-min_x_axis = math.inf
 
 # initilizes the config of plt
-initilize_plt_conf(plt)
+colors = ['b', 'g', 'r', 'm', 'y', 'c']
+f = plt.figure(1)
+create_graph(target="mod_reward",plt_name=f,y_label="reward",x_label="step",result_folders=result_folders
+             ,colors=colors)
+#### exploration graph in polyrl ####
+g = plt.figure(2)
 
-for c, folder in enumerate(result_folders):
-    name_folder = folder.split("/")[-2]
-    line_names.append(name_folder)
-    min_y_axis=math.inf
-    results = []
-    files = glob(folder + "/*.pkl")
-    x = get_x(files[0])
-    for file_path in files:
-        last_x,last_y = get_result_file(results, file_path)
-        if (last_x < min_x_axis):
-            min_x_axis = last_x
-        if (last_y < min_y_axis):
-            min_y_axis = last_y
-    x=x[:min_y_axis]
-    normalized_results=[]
-    for data in results:
-        normalized_results.append(data[:min_y_axis])
-    mean = np.mean(np.array(normalized_results), axis=0)
-    std = np.std(np.array(normalized_results), axis=0)
-    plt.plot(x, mean, colors[c],label=name_folder)
-    plt.fill_between(x, mean - std, mean + std, edgecolor=colors[c],
-                     facecolor=colors[c], alpha=0.21,
-                     linewidth=0)
-    dict_results[name_folder] = {"mean": mean, "std": std}
-
-plt.legend(loc='upper left')
-axes = plt.gca()
-plt.show()
