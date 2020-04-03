88c3cb770419d5f9eb130c6f724377ee982193db
habout <hossain.aboutalebi@gmail.com>
2020-04-03 19:40:27 -0400

    Added OAC 2

--- engine/algorithms/OAC/oac.py
+++ engine/algorithms/OAC/oac.py
@@ -10,35 +10,8 @@ class OAC(SAC):
 
     def __init__(self, state_dim, action_dim, max_action, action_space, gamma, tau, alpha, device, update_interval=1,
                  policy="Gaussian", automatic_entropy_tuning=False, hidden_size=256, lr=0.0003, start_steps=10000):
-        super(OAC, self).__init__(state_dim=state_dim, action_dim=action_dim,
-                                  max_action=max_action,gamma=gamma,tau=tau,
-                                  alpha=alpha,device=device,
-                                  policy=policy, start_steps=start_steps)
-        self.automatic_entropy_tuning = automatic_entropy_tuning
-        self.critic = QNetwork(state_dim, action_dim, hidden_size).to(device=self.device)
-        self.critic_optim = Adam(self.critic.parameters(), lr=lr)
-        self.action_space = action_space
-
-        self.critic_target = QNetwork(state_dim, action_dim, hidden_size).to(self.device)
-        hard_update(self.critic_target, self.critic)
-
-        if self.policy_type == "Gaussian":
-            # Target Entropy = −dim(A) (e.g. , -6 for HalfCheetah-v2) as given in the paper
-            if self.automatic_entropy_tuning == True:
-                self.target_entropy = -torch.prod(torch.Tensor(action_space.shape).to(self.device)).item()
-                self.log_alpha = torch.zeros(1, requires_grad=True, device=self.device)
-                self.alpha_optim = Adam([self.log_alpha], lr=lr)
-
-            self.policy = GaussianPolicy(state_dim, action_dim, hidden_size, action_space).to(
-                self.device)
-            self.policy_optim = Adam(self.policy.parameters(), lr=lr)
-
-        else:
-            self.alpha = 0
-            self.automatic_entropy_tuning = False
-            self.policy = DeterministicPolicy(state_dim, action_space.shape[0], hidden_size, action_space).to(
-                self.device)
-            self.policy_optim = Adam(self.policy.parameters(), lr=lr)
+        super(OAC, self).__init__(state_dim, action_dim, max_action, action_space=action_space,
+                    gamma=gamma, alpha=alpha, tau=tau, policy=policy, device=device, start_steps=start_steps)
 
     def select_action(self, state, tensor_board_writer=None, step_number=None):
         self.counter_actions += 1
